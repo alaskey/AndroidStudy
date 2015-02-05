857847072a49fff7727c8bda722f3dca1df3dfdc
yiwen.tang <yiwen.tang@ck-telecom.com>
2015-02-05 09:56:30 +0800

    define TAG in MainActivity
    [Solution Description]
    modified:   hello/src/main/java/com/example/ckt/hello/MainActivity.java
    [Other info]
    N/A

--- Android-Study-2015/hello/src/main/java/com/example/ckt/hello/MainActivity.java
+++ Android-Study-2015/hello/src/main/java/com/example/ckt/hello/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
-
+    public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
