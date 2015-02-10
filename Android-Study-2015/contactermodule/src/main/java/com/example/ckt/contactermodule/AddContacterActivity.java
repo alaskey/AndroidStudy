80720c7e028e6b7c57f920dae1ec428f27c3b7c1
yiwen.tang <yiwen.tang@ck-telecom.com>
2015-02-10 17:48:37 +0800

    this commit is to study more in layout, and realize a address list but not complish.
    [Solution Description]
    
    new file:   contactermodule/.gitignore
    new file:   contactermodule/build.gradle
    new file:   contactermodule/contactermodule.iml
    new file:   contactermodule/proguard-rules.pro
    new file:   contactermodule/src/androidTest/java/com/example/ckt/contactermodule/ApplicationTest.java
    new file:   contactermodule/src/main/AndroidManifest.xml
    new file:   contactermodule/src/main/java/com/example/ckt/contactermodule/AddContacterActivity.java
    new file:   contactermodule/src/main/java/com/example/ckt/contactermodule/MainActivity.java
    new file:   contactermodule/src/main/res/drawable-hdpi/contacter.png
    new file:   contactermodule/src/main/res/drawable-hdpi/ic_launcher.png
    new file:   contactermodule/src/main/res/drawable-hdpi/ok.png
    new file:   contactermodule/src/main/res/drawable-mdpi/ic_launcher.png
    new file:   contactermodule/src/main/res/drawable-xhdpi/ic_launcher.png
    new file:   contactermodule/src/main/res/drawable-xxhdpi/ic_launcher.png
    new file:   contactermodule/src/main/res/layout/activity_addcontacter.xml
    new file:   contactermodule/src/main/res/layout/activity_main.xml
    new file:   contactermodule/src/main/res/menu/menu_main.xml
    new file:   contactermodule/src/main/res/values-w820dp/dimens.xml
    new file:   contactermodule/src/main/res/values/dimens.xml
    new file:   contactermodule/src/main/res/values/strings.xml
    new file:   contactermodule/src/main/res/values/strings_spinner.xml
    new file:   contactermodule/src/main/res/values/styles.xml
    
    [other info]
    N / A

---
+++ Android-Study-2015/contactermodule/src/main/java/com/example/ckt/contactermodule/AddContacterActivity.java
@@ -0,0 +1,101 @@
+package com.example.ckt.contactermodule;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ckt on 2/10/15.
+ */
+public class AddContacterActivity extends Activity implements View.OnClickListener {
+
+    private Spinner spTel;
+    private Spinner spAddress;
+    private Spinner spDate;
+
+    private Button btnOk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_addcontacter);
+
+        findViews();
+
+        initUI();
+
+        btnOk.setOnClickListener(this);
+    }
+
+    private void findViews() {
+
+        spTel = (Spinner) findViewById(R.id.spTel);
+        spAddress = (Spinner) findViewById(R.id.spAddress);
+        spDate = (Spinner) findViewById(R.id.spDate);
+
+        btnOk = (Button) findViewById(R.id.btnOk);
+    }
+
+    private void initUI() {
+
+        // telephone
+        List<String> telList = new ArrayList<String>();
+        telList.add("telphone");
+        telList.add("at home");
+        telList.add("at company");
+
+        ArrayAdapter<String> telAdapter = new ArrayAdapter<String>(AddContacterActivity.this, android.R.layout.simple_spinner_item, telList);
+        telAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spTel.setAdapter(telAdapter);
+        spTel.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        // address
+        String[] address = getResources().getStringArray(R.array.address);
+
+        ArrayAdapter<String> addressAdapter = new ArrayAdapter<String>(AddContacterActivity.this, android.R.layout.simple_spinner_item, address);
+
+        spAddress.setAdapter(addressAdapter);
+        spAddress.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        // date
+        String [] date = getResources().getStringArray(R.array.date);
+        ArrayAdapter<String> dateAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,date);
+        spDate.setAdapter(dateAdapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnOk:
+                break;
+        }
+    }
+}
